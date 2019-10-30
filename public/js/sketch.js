2f98c3ac3f8aa85456659e49e83c24250827b8e4
JetBlack <jetb250@gmail.com>
2019-10-29 23:27:24 -0500

    Adjusted background scale Added interactivity to point table

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,32 +1,41 @@
 /// <reference path="./p5.global-mode.d.ts" />
 
-var bg, screenHeight = 319, screenWidth = 649;
+var bg, multiplier = 2,
+    screenHeight = 1298,
+    screenWidth = 638;
 var btnAdd, btnRemove, radius = 5;
 var trajectory;
 var lastActive = null;
 var active = false;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    bg = loadImage('assets/field.png');
+    var divPoint = document.getElementById('points');
+    createCanvas(screenHeight, screenWidth);
+    dragElement(divPoint);
+    divPoint.style.top = 0;
+    divPoint.style.right = 0;
+    bg = loadImage('public/assets/field1298x638.jpeg');
     trajectory = new Trajectory();
 }
 
 function draw() {
-    background('white');
+    background(bg);
     trajectory.draw();
 }
 
 function pointAdd() {
-    trajectory.add();
+    trajectory.add(screenHeight / 2, screenWidth / 2);
+    tableUpdate();
 }
 
 function pointRemove() {
-    trajectory.remove(trajectory.points.indexOf(lastActive));
+    var i = trajectory.points.indexOf(lastActive);
+    trajectory.remove(i !== -1 ? i : trajectory.points.length - 1);
+    tableUpdate();
 }
 
 function tableUpdate() {
-    var tbl, tbody, tr, tdi, tdx, tdy;
+    var tbl, tbody, tr, tdi, tdx, tdy, inputx, inputy;
     tbl = document.getElementById('tbl-points');
     tbody = tbl.tBodies[0];
 
@@ -40,6 +49,14 @@ function tableUpdate() {
                 tdi = tr.insertCell(0);
                 tdx = tr.insertCell(1);
                 tdy = tr.insertCell(2);
+                inputx = document.createElement('input');
+                inputy = document.createElement('input');
+                inputx.oninput = tableSync;
+                inputy.oninput = tableSync;
+                inputx.onclick = inputx.focus;
+                inputy.onclick = inputy.focus;
+                tdx.appendChild(inputx);
+                tdy.appendChild(inputy);
                 tdx.className = "data";
                 tdy.className = "data";
             }
@@ -53,8 +70,8 @@ function tableUpdate() {
         for (var i = 0; i < trajectory.points.length; i++) {
             tr = tbody.rows[i];
             tr.cells[0].innerHTML = i;
-            tr.cells[1].innerHTML = trajectory.points[i].x;
-            tr.cells[2].innerHTML = trajectory.points[i].y;
+            tr.cells[1].firstChild.value = Math.ceil(trajectory.points[i].x / multiplier);
+            tr.cells[2].firstChild.value = Math.ceil(trajectory.points[i].y / multiplier);
         }
     } else {
         while (tbody.hasChildNodes()) {
@@ -64,6 +81,20 @@ function tableUpdate() {
         td = tr.insertCell(0);
         td.setAttribute('colspan', '3');
         td.innerHTML = "No set points";
+    }
+}
+
+function tableSync() {
+    var tbl, tbody, tr, waypoint;
+    tbl = document.getElementById('tbl-points');
+    tbody = tbl.tBodies[0];
+
+    for (var r = 0; r < tbody.rows.length; r++) {
+        tr = tbody.rows[r];
+        waypoint = trajectory.points[r];
+
+        waypoint.x = parseInt(tr.cells[1].firstChild.value) * multiplier || 0;
+        waypoint.y = parseInt(tr.cells[2].firstChild.value) * multiplier || 0;
     }
 }
 
@@ -90,6 +121,14 @@ function mousePressed() {
         }
     }
     return false;
+}
+
+function mouseReleased() {
+    for (var i = 0; i < trajectory.points.length; i++) {
+        if (trajectory.points[i].active) {
+            tableUpdate();
+        }
+    }
 }
 
 function mouseDragged() {
--- public/js/trajectory.js
+++ public/js/trajectory.js
@@ -12,17 +12,24 @@ class Waypoint extends Point {
         this.radius = radius;
         this.active = false;
     }
+    
+    setActive() {
+        this.active = true;
+    }
 
     draw() {
+        stroke('black');
+        fill('red');
+        circle(this.x, this.y, this.radius * 2);
         noStroke();
         fill('black');
-        ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
         textSize(15);
         text('K', this.x + 10, this.y + 10);
         textSize(10);
         text(this.i, this.x + 20, this.y + 15);
         if (this.active) {
             stroke('red');
+            noFill();
             rect(this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
         }
     }
@@ -53,8 +60,8 @@ class Curve {
             stroke('red');
             strokeWeight(2);
             line(x, y, cos(a) * 50 + x, sin(a) * 50 + y);
-            stroke('black');
-            ellipse(x, y, 5, 5);
+            stroke('white');
+            circle(x, y, 5);
         }
     }
 }
@@ -65,15 +72,32 @@ class Trajectory {
         this.curves = [];
     }
 
+    add(x, y) {
+        var waypoint = new Waypoint(this.points.length, x, y);
+        this.points.push(waypoint);
+        this.update();
+    }
+
+    remove(i) {
+        if (this.points.length > 0) {
+            this.points.splice(i, 1);
+            this.curves.splice(i - 1, 1);
+        }
+        for (var i = 0; i < this.points.length; i++) {
+            this.points[i].i = i;
+        }
+        this.update();
+    }
+
     update() {
         var n = this.points.length - 1;
         if (n > 0) {
             this.curves = Array(n);
-    
+
             for (var i = 0; i < n; i++) {
                 this.curves[i] = new Curve();
             }
-            
+
             if (n == 1) {
                 this.curves[0].p0 = this.points[0];
                 this.curves[0].p1 = this.points[0];
@@ -82,24 +106,27 @@ class Trajectory {
             } else if (n > 1) {
                 var mat, dx, dy, p1x, p1y, p2;
                 if (n == 2) {
-                    mat = [[2, 1], [2, 7]];
-            
+                    mat = [
+                        [2, 1],
+                        [2, 7]
+                    ];
+
                     dx = [this.points[0].x + 2 * this.points[1].x, 8 * this.points[1].x + this.points[2].x];
-                    dy = [this.points[0].y + 2 * this.points[1].y, 8 * this.points[1].y + this.points[2].y]; 
+                    dy = [this.points[0].y + 2 * this.points[1].y, 8 * this.points[1].y + this.points[2].y];
                 } else {
                     dx = [this.points[0].x + 2 * this.points[1].x];
                     dy = [this.points[0].y + 2 * this.points[1].y];
-    
+
                     for (var i = 1; i < n - 1; i++) {
                         dx.push(4 * this.points[i].x + 2 * this.points[i + 1].x);
                         dy.push(4 * this.points[i].y + 2 * this.points[i + 1].y);
                     }
-    
+
                     dx.push(8 * this.points[n - 1].x + this.points[n].x);
                     dy.push(8 * this.points[n - 1].y + this.points[n].y);
-    
+
                     mat = Array(n);
-                    
+
                     for (var i = 0; i < n; i++) {
                         mat[i] = Array(n).fill(0);
                     }
@@ -108,25 +135,25 @@ class Trajectory {
                         mat[i][i] = 4;
                         mat[i][i + 1] = 1;
                     }
-    
+
                     mat[0][0] = 2;
                     mat[0][1] = 1;
                     mat[n - 1][n - 2] = 2;
-                    mat[n - 1][n - 1] = 7; 
+                    mat[n - 1][n - 1] = 7;
                 }
-    
+
                 p1x = math.multiply(math.inv(mat), dx);
-                p1y = math.multiply(math.inv(mat), dy);        
-    
+                p1y = math.multiply(math.inv(mat), dy);
+
                 p2 = [];
-    
+
                 for (var i = 0; i < n; i++) {
                     p2.push(new Point(2 * this.points[i + 1].x - p1x[i + 1], 2 * this.points[i + 1].y - p1y[i + 1]))
                 }
-    
+
                 p2[n - 1].x = 0.5 * (this.points[n].x + p1x[n - 1]);
                 p2[n - 1].y = 0.5 * (this.points[n].y + p1y[n - 1]);
-    
+
                 for (var i = 0; i < n; i++) {
                     this.curves[i].p0 = this.points[i];
                     this.curves[i].p1 = new Point(p1x[i], p1y[i]);
@@ -137,28 +164,11 @@ class Trajectory {
         }
     }
 
-    add() {
-        var waypoint = new Waypoint(this.points.length, screenWidth / 2, screenHeight / 2);
-        this.points.push(waypoint);
-        this.update();
-    }
-
-    remove(i) {
-        if (i !== -1) {
-            this.points.splice(i, 1);
-            this.curves.splice(i - 1, 1);
-        }
-        for (var i = 0; i < this.points.length; i++) {
-            this.points[i].i = i;
-        }
-        this.update();
-    }
-
     draw() {
         for (var i = 0; i < this.points.length; i++) {
             this.points[i].draw();
         }
-    
+
         for (var i = 0; i < this.curves.length; i++) {
             this.curves[i].draw();
         }
